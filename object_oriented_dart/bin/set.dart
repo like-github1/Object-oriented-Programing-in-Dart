a9f676f8b7dd684d8516134108e9de8fe21e739a
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-05 02:35:22 -0700

    dart

---
+++ object_oriented_dart/bin/set.dart
@@ -0,0 +1,14 @@
+void main() {
+  //set is Unorder Collection of elements
+
+  var obj1 = person();
+  print(obj1());
+
+//Callable Class in Dart
+}
+
+class person {
+  String call() {
+    return "this is a callable class,of ";
+  }
+}
